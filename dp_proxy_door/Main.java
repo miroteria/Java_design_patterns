ddeadf62f19ad53bbe97f086047bc77b61fd7feb
miroteria <miroslav.simi3@gmail.com>
2025-08-28 21:33:11 +0200

    Update Main.java

--- dp_proxy_door/Main.java
+++ dp_proxy_door/Main.java
@@ -1,1 +1,9 @@
-
+public class Main {
+	
+	public static void main(String[] args) {
+		Door door = new DoorProxy(new DoorLock());
+		door.open("abraca-dabra-door-open");
+		door.open("123456");
+		door.close();
+	}
+}
